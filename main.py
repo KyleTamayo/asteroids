c1d764b685e5908feb307873abc064295d43e5f4
kyletamayo <kyle_tamayo@startmail.com>
2024-10-26 19:06:42 -0400

    Updates

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	
+
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+	clock = pygame.time.Clock()
+	dt = 0
+	
 	while(True):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -16,6 +19,9 @@ def main():
 
 		screen.fill("black")
 		pygame.display.flip()
+
+		dt = 1000 / clock.tick(60)
+
 
 if __name__ == "__main__":
 	main()
